6ba724c3826bb6518a894a4d3f25c97cab13713e
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-08-17 15:23:35 -0500

    Add basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './courses/summary.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, SummaryPipe],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    SummaryPipe,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent,
+    NoAccessComponent,
+    NotFoundComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'no-access', component: NoAccessComponent },
+    ]),
+  ],
+  providers: [OrderService, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
